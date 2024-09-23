e438582d05737715dcb46b2deb6c1df703248a5d
Hoàng Lố Data <nm.hoang19@gmail.com>
2024-09-23 11:57:05 +0700

    prep pull code v2

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home/home.html')
+    items = [{'id': i, 'name': f'Item {i}', 'description': f'This is description for item {i}'} for i in range(1, 11)]
+    return render(request, 'home/home.html', {'items': items})
